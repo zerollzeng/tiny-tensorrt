29dfb380a811eda8c69919e22c3bece0a4282a79
zerollzeng <zerollzeng@gmail.com>
2022-01-03 14:34:29 +0800

    minor change on sampleINT8

--- samples/sampleINT8/sampleINT8.cpp
+++ samples/sampleINT8/sampleINT8.cpp
@@ -8,7 +8,7 @@ int main() {
 
     onnx_net->EnableFP16();
     onnx_net->EnableINT8();
-    onnx_net->SetInt8Calibrator("EntropyCalibrator", 1, "/tmp/calibrate_data/", "/tmp/resnet50_int8.cache");
+    onnx_net->SetInt8Calibrator("EntropyCalibratorV2", 1, "/tmp/calibrate_data/", ""/*calibrateCachePath*/);
     onnx_net->AddDynamicShapeProfile("gpu_0/data_0", {1,3,224,224}, {1,3,224,224}, {1,3,224,224});
     // build engine
     onnx_net->BuildEngine("/usr/src/tensorrt/data/resnet50/ResNet50.onnx", "/tmp/resnet50.plan");
